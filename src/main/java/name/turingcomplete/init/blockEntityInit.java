f9527145ad4ee5e1c8c02fef69638d28b0d24c0c
DerVogel101 <adminosaurus@dervogel101.de>
2026-01-10 00:00:23 +0100

    Refactor `register` method to use `FabricBlockEntityTypeBuilder` for block entity registration

--- src/main/java/name/turingcomplete/init/blockEntityInit.java
+++ src/main/java/name/turingcomplete/init/blockEntityInit.java
@@ -2,6 +2,7 @@ package name.turingcomplete.init;
 
 import name.turingcomplete.TuringComplete;
 import name.turingcomplete.blocks.truthtable.TruthTableBlockEntity;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
@@ -12,8 +13,8 @@ import net.minecraft.registry.Registry;
 public class blockEntityInit {
     public static BlockEntityType<TruthTableBlockEntity> TRUTH_TABLE_BLOCK_ENTITY = register("truth_table", blockInit.TRUTH_TABLE, TruthTableBlockEntity::new);
 
-    public static <T extends BlockEntity> BlockEntityType<T> register(String id, Block block, BlockEntityType.BlockEntityFactory<T> factory) {
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, TuringComplete.id(id), BlockEntityType.Builder.create(factory, block).build());
+    public static <T extends BlockEntity> BlockEntityType<T> register(String id, Block block, FabricBlockEntityTypeBuilder.Factory<T> factory) {
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, TuringComplete.id(id), FabricBlockEntityTypeBuilder.create(factory, block).build());
     }
 
     public static void load(){}
