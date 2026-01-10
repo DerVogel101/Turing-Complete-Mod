cdee48e00d643a2617eefb7f1836fb7e4b41f233
DerVogel101 <adminosaurus@dervogel101.de>
2026-01-10 16:26:35 +0100

    Update BlockRenderLayer usage for transparency settings

--- src/client/java/name/turingcomplete/TuringCompleteClient.java
+++ src/client/java/name/turingcomplete/TuringCompleteClient.java
@@ -5,20 +5,20 @@ import name.turingcomplete.client.screen.TruthTableScreen;
 import name.turingcomplete.color.BlockTint;
 import name.turingcomplete.init.blockInit;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class TuringCompleteClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// To make some parts of the block transparent (like glass, saplings and doors):
-		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(),
+		BlockRenderLayerMap.putBlocks(BlockRenderLayer.CUTOUT,
 				blockInit.NOT_GATE,blockInit.NAND_GATE,
 				blockInit.NOR_GATE,blockInit.XNOR_GATE
 		);
 
-		BlockRenderLayerMap.INSTANCE.putBlock(blockInit.OMNI_DIRECTIONAL_REDSTONE_BRIDGE_BLOCK,RenderLayer.getTranslucent());
+		BlockRenderLayerMap.putBlock(blockInit.OMNI_DIRECTIONAL_REDSTONE_BRIDGE_BLOCK, BlockRenderLayer.TRANSLUCENT);
 
 		BlockTint.create();
 
